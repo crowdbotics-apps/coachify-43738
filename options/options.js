db7d5b5ac292607cb798a920d232c7ff8a39a35c
Crowdbotics <team@crowdbotics.com>
2023-10-03 17:54:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "coachify-43738",
+  url: "https://coachify-43738.botics.co",
+  api: "https://coachify-43738.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
